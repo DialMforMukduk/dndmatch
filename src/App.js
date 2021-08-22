35e16d33cb942a3f6ea74f018bdf59ef31567bb8
Jared <carpenter.jared92@gmail.com>
2021-08-22 17:05:15 -0600

    init
    inital

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          The inital commit
+          The inital commit. Part 1
         </p>
         <a
           className="App-link"

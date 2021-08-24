b603322544278c1240aa94d773c383f28143afcd
Jared <carpenter.jared92@gmail.com>
2021-08-24 15:52:03 -0600

    wrapping auth
    setting up auth flow with amplify

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+import { withAuthenticator } from '@aws-amplify/ui-react'
+
+Amplify.configure(awsExports);
 
 function App() {
   return (
@@ -22,4 +27,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App);
